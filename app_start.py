922a8cc232eb5055e753efee52581f0bbe9f41db
roopesh2013 <roopeshfrancis@yahoo.co.in>
2020-04-14 20:13:32 +0800

    latest

--- app_start.py
+++ app_start.py
@@ -2,6 +2,31 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-	return "hello world"
+	user = {'username': 'There'}
+	return '''
+<html>
+    <head>
+        <title>Index Page</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+        <p> This is to test a flask app deployed using jenkins, docker and ansible </p>
+    </body>
+
+
+</html>'''
+@app.route('/index')
+def index():
+    user = {'username': 'There'}
+    return '''
+<html>
+    <head>
+        <title>Index Page</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0',port='7000')
